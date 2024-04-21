8ac3950513539b966f332ed53db002001f3c4099
Meer Quais <meerquais017@gmail.com>
2024-04-21 11:12:09 +0500

    class 1
    express intro, server running, basic routing

---
+++ classIntro.js
@@ -0,0 +1,10 @@
+console.log("AOA")
+const arr = [1, 2, 3, 4, 5]
+console.log(arr)
+console.log("my new comment")
+console.log("first")
+// make folder
+// npm init
+// start command in package.json    "start": "node index.js",
+// index.js
+//run console
